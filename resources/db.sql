5d52c7a67000f02ea37b933beec22cd5fe1a65fc
Robert Hampton <robbzilla@byui.edu>
2020-05-27 21:29:15 -0500

    Initial dashboard pages

---
+++ resources/db.sql
@@ -0,0 +1,18 @@
+-- clean up
+
+DROP TABLE IF EXISTS portfolio;
+
+
+
+-- setup
+
+CREATE TABLE portfolio (
+	id SERIAL PRIMARY KEY,
+
+);
+
+
+
+-- initial values
+
+INSERT INTO portfolio (id) VALUES (1);
